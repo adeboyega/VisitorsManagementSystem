8379d7cf1a3a61c0c4939c26b6e3fdff7fd70bfd
Bayomi Aremo <8955yomi@gmail.com>
2024-02-15 17:18:33 -0700

    recomendations done ✔

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CiMail } from "react-icons/ci";
 import { CiLock } from "react-icons/ci";
 import { EmblaCarousel } from "../EmblaCarousel";
+import { IoPersonAddOutline } from "react-icons/io5";
 
 const Login = () => {
 	return (
@@ -10,8 +11,15 @@ const Login = () => {
 			<h1 className=" font-bold text-2xl text-gray-100">Login</h1>
 			<p className="flex gap-2 text-gray-100 ">
 				{" "}
-				<CiMail /> name
+				<IoPersonAddOutline /> Rank
 			</p>
+			
+			<input type="text" className="w-10 h-10 text-gray-100" />
+			<p className="flex gap-2 text-gray-100 ">
+				{" "}
+				<CiMail /> Name
+			</p>
+			
 			<input type="text" className="w-10 h-10 text-gray-100" />
 			<p className="flex gap-2 text-sm text-gray-100">
 				<CiLock /> Password
